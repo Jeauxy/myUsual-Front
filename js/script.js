2d73ab778c72470354aa6d857c049f485c13941f
Joseph Callaway <jeauxcal@gmail.com>
2016-10-24 15:45:01 -0500

    FIXED Front End Auth0 user sign in. Adjusted index & script files

--- js/script.js
+++ js/script.js
@@ -1,125 +1,136 @@
-
-// AUTH0 LOCK FOR CUSTOM LOGIN
-
-// var lock = new Auth0Lock('GoBNjyrd7W9Jg1HECE7nH82QUhjTsM2B', 'jeauxy.auth0.com', {
-//     additionalSignUpFields: [{
-//       name: "address",                              // required
-//       placeholder: "Enter your address",            // required
-//       icon: "https://example.com/address_icon.png", // optional
-//       validator: function(value) {                  // optional
-//         // only accept addresses with more than 10 characters
-//         return value.length > 10;
-//       }
-//     }]
-//   });
-//
-// var showUserProfile = function(profile) {
-//
-//   if (profile.hasOwnProperty('user_metadata')) {
-//     $('#address').text(profile.user_metadata.address);
-//   }
-// }
-
 $(document).ready(function() {
-  var auth0 = null;
-  auth0 = new Auth0({
-    domain: 'jeauxy.auth0.com',
-    clientID: 'GoBNjyrd7W9Jg1HECE7nH82QUhjTsM2B',
-    callbackOnLocationHash: true,
-    responseType: 'token',
-    callbackURL: './',
-  });
 
-  $('#btn-login').on('click', function(e) {
-    console.log("LOGIN CLICKED");
+  $('#btn-login').on('click', function (e) {
     e.preventDefault();
-    var username = $('#username').val();
-    var password = $('#password').val();
-    auth0.login({
-      connection: 'Username-Password-Authentication',
-      responseType: 'token',
-      email: username,
-      password: password,
-    }, function(err) {
-      if (err) {
-        alert("something went wrong: " + err.message);
-      } else {
-        show_logged_in(username);
-      }
-    });
+    lock.show();
   });
 
-  $('#btn-register').on('click', function(e) {
-    console.log("SIGN UP CLICKED");
+  $('#logout').on('click', function (e) {
     e.preventDefault();
-    var username = $('#username').val();
-    var password = $('#password').val();
-    auth0.signup({
-      connection: 'Username-Password-Authentication',
-      responseType: 'token',
-      email: username,
-      password: password,
-    }, function(err) {
-      if (err) alert("something went wrong: " + err.message);
-    });
+    logout();
   });
 
-  $('#btn-google').on('click', function(ev) {
-    console.log("GOOGLE CLICKED");
-    ev.preventDefault();
-    auth0.login({
-      connection: 'google-oauth2'
-    }, function(err) {
-      if (err) alert("something went wrong: " + err.message);
-    });
-  });
-
-  $('#btn-logout').on('click', function(ev) {
-    console.log("LOGOUT CLICKED");
-     ev.preventDefault();
-     localStorage.removeItem('id_token');
-     window.location.href = "/";
-  })
-
-  var parseHash = function() {
-    var token = localStorage.getItem('id_token');
-    if (null != token) {
-      show_logged_in();
-    } else {
-      var result = auth0.parseHash(window.location.hash);
-      if (result && result.idToken) {
-        localStorage.setItem('id_token', result.idToken);
-        show_logged_in();
-      } else if (result && result.error) {
-        alert('error: ' + result.error);
-        show_sign_in();
-      }
-    }
-  };
-
-  parseHash();
+  if (isLoggedIn()){
+    loadFoods();
+    showProfile();
+    addNewFood();
+    $('#welcome').hide();
+  }
 
 });
 
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://${account.namespace}/dbconnections/signup",
-  "method": "POST",
-  "headers": {
-    "content-type": "application/json"
-  },
-  "processData": false,
-  "data": "{\"client_id\": \"GoBNjyrd7W9Jg1HECE7nH82QUhjTsM2B\",\"email\": \"$('#signup-email').val()\",\"password\": \"$('#signup-password').val()\",\"user_metadata\": {\"name\": \"john\",\"color\": \"red\"}}"
+function deleteFood(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  var link = $(this)
+  $.ajax({
+    url: link.attr('href'),
+    method: 'DELETE',
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem('idToken')
+    }
+  }).done(function () {
+    link.parent('li').remove();
+  })
 }
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+function showProfile() {
+  $('#btn-login').hide();
+  $('#user-info').show();
+  lock.getProfile(localStorage.getItem('idToken'), function (error, profile) {
+    if (error){
+      logout();
+    } else {
+      console.log('profile', profile);
+      $('#fullName').text(profile.given_name);
+    }
+  })
+}
+
+function addNewFood() {
+  $('#new-food-form').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+      url: 'https://boiling-wildwood-13698.herokuapp.com/foods',
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('idToken')
+      },
+      data: {
+        text: $('#food-name').val()
+      }
+    }).done(function (newFood) {
+      loadFood(newFood)
+      $('#food-name').val('').focus()
+    })
+  })
+}
+
+function loadFoods() {
+  $.ajax({
+    url: 'https://boiling-wildwood-13698.herokuapp.com/foods',
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem('idToken')
+    }
+  }).done(function (data) {
+    data.forEach(function (datum) {
+      loadFood(datum)
+  })
+})
+}
+
+function loadFood(food) {
+    var li = $('<li />')
+    li.text(itemName.text + ' ')
+    li.data('id', itemName._id);
+    if (itemName.text){
+      li.addClass('done');
+    }
+
+    $('#foods').append(li);
+}
+
+var lock = new
+//1. Client ID, 2. Client Domain, 3. Oject of Attr
+  Auth0Lock('GoBNjyrd7W9Jg1HECE7nH82QUhjTsM2B', 'jeauxy.auth0.com', {
+    auth: {
+      params: {
+        scope: 'openid email'
+    }
+  }
 });
 
-window.auth0 = new Auth0({
-  domain: 'jeauxy.auth0.com',
-  clientID: 'GoBNjyrd7W9Jg1HECE7nH82QUhjTsM2B',
-  // Callback made to your server's callback endpoint
-  callbackURL: './',
+function isLoggedIn() {
+  var token = localStorage.getItem('idToken')
+  var expired = isJwtValid(token);
+
+  if (token) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+function isJwtValid(token) {
+  var token = localStorage.getItem('idToken');
+  if (!token){
+    return false;
+  }
+}
+
+
+lock.on('authenticated', function (authResult) {
+  console.log(authResult);
+  localStorage.setItem('idToken', authResult.idToken);
+  console.log('Logged In!');
+  loadFoods();
+  showProfile();
+  addNewFood();
+  $('#welcome').hide();
 });
+
+function logout() {
+    localStorage.removeItem('idToken')
+    window.location.href = '/';
+}
