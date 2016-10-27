116004844811d08bdb595ac4ae502b55352f48e8
Joseph Callaway <jeauxcal@gmail.com>
2016-10-27 14:31:33 -0500

    Fixing logout href link

--- js/script.js
+++ js/script.js
@@ -447,5 +447,5 @@ lock.on('authenticated', function (authResult) {
 
 function logout() {
   localStorage.removeItem('idToken')
-  window.location.href = '/myUsual-Front/';
+  window.location.href = './myUsual-Front/';
 };
